4a29859653888091bac6da9a5009a31229893990
박민식 <jaden2973@naver.com>
2025-08-24 23:15:57 +0900

    11053 cpp solved for daily baekjun 250824

---
+++ cpp/silver2/Longest11053.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<int> arr(N);
+
+    vector<int> dp(N, 1);
+
+    for(int i = 0; i < N; i++){
+        cin >> arr[i];
+    }
+
+    int answer = 1;
+    for(int i = 0; i < N; i++){
+        for(int j=0; j < i; j++){
+            if (arr[j] < arr[i]) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+        answer = max(answer, dp[i]);
+    }
+    cout << answer << endl;
+    return 0;
+}
+
