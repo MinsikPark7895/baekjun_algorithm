6f79bb89c9fa9401af86cc4e320c489b3b00f100
박민식 <jaden2973@naver.com>
2025-08-31 23:42:39 +0900

    2644 python solved for daily baekjun 250831

--- python/silver2/relative2644.py
+++ python/silver2/relative2644.py
@@ -1,7 +1,31 @@
-n = int(input())
+import sys
+from collections import deque
 
-p1, p2 = map(int, input().split())
+n = int(input().strip())
+a, b = map(int, input().split())
+m = int(input().strip())
 
-m = int(input())
+g = [[] for _ in range(n + 1)]
 
-graph = []
+for _ in range(m):
+    x, y = map(int, input().split())
+
+    g[x].append(y)
+    g[y].append(x)
+
+
+dist = [-1] * (n + 1)
+q = deque([a])
+dist[a] = 0
+
+while q:
+    u = q.popleft()
+    if u == b:
+        break
+    for v in g[u]:
+        if dist[v] == -1:
+            dist[v] = dist[u] + 1
+            q.append(v)
+
+print(dist[b])
+
