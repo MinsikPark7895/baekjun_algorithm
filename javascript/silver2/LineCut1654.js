59ea8d7fd7fed08b58e4f6fa812de0cc9c02a9d6
박민식 <jaden2973@naver.com>
2026-01-07 17:50:48 +0900

    imcomplete 1654 baekjun 260107

--- javascript/silver2/LineCut1654.js
+++ javascript/silver2/LineCut1654.js
@@ -4,42 +4,22 @@ const lines = input.split('\n')
 
 const [K, N] = lines[0].split(" ").map(Number);
 
-let total_length = 0
+const arr = new Array(K);
 
-for(let i = 0; i < K; i++) {
-    total_length += Number(lines[i + 1])
+let max_length = 0
+
+for (let i = 0; i < K; i++) {
+    arr[i] = Number(lines[i + 1])
+    if (max_length < arr[i]) {
+        max_length = arr[i]
+    }
 }
 
-let max_length = Math.floor(total_length / N)
+let left = 1;
+let right = max_length;
+let ans = 0;
 
-let current_num = 0
-
-function two_pointer(start, end ,target) {
+while (left <= right) {
     
-    flag = true
-    
-    while (flag) {
-        cur_start = start
-        cur_end = end
-        mid = Math.floor((cur_start + cur_end) / 2)
-
-        
-
-        flag = false
-    }
 }
-
-while (current_num !== N){   
-    current_num = 0
-    for (let i = 0; i < K; i++){
-        current_num += Math.floor(Number(lines[i + 1]) / max_length)
-    }
-    if(current_num !== N) {
-        max_length -= 1
-    }
-}
-
-console.log(max_length)
-
-
 
