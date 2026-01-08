49e9e0d4dde4608413251764a8e0c397eecc5354
박민식 <jaden2973@naver.com>
2026-01-09 00:04:58 +0900

    incomplete 2493 cpp for daily baekjun 260109

--- cpp/gold5/Top2493.cpp
+++ cpp/gold5/Top2493.cpp
@@ -1,11 +1,24 @@
 // 맨 오른쪽에서 가장 높은 타워를 찾는 것
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+
+
 int main(void) {
+    int N;
     
+    cin >> N;
+
+    vector<int> arr(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+
 
     return 0;
 }
