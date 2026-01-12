f716e9741ff8d62d59a2bd2b6f54685866f9e89d
박민식 <jaden2973@naver.com>
2026-01-12 23:46:09 +0900

    7579 cpp solved for daily baekjun 260112

--- cpp/gold3/App7579.cpp
+++ cpp/gold3/App7579.cpp
@@ -1,52 +1,56 @@
 #include <iostream>
 #include <vector>
-#include <climits>
+#include <algorithm>
 
 using namespace std;
 
-int result = INT_MAX; // 출력값에 정수 최대 범위 수 지정
-
-int N, M;  // N: 숫자의 개수, M: 필요한 메모리
-
-vector<int> required_memory;  // 필요한 메모리 배열
-vector<int> additional_memory;  // 활성화에 필요한 추가 메모리 배열
-
-// 백트래킹을 통한 추적
-void min_btye(int idx, int cur_sum, int cur_result) {
-    // 필요한 메모리 이상의 값을 넘겨 받았을 경우
-    if (cur_sum >= M) {
-        result = min(result, cur_result);
-        return;
-    }
-
-    // 들어온 인덱스를 기준으로 필요한 
-    for (int i = idx; i < N; i++) {
-        if (idx < N - 1) {
-            min_btye(i + 1, cur_sum + required_memory[i], cur_result + additional_memory[i]);
-        }
-    }
-
-    return;
-}
-
 int main(void) {
+
+    int N, M;
 
     cin >> N >> M;
 
-    required_memory.resize(N);
-    additional_memory.resize(N);
+    vector<int> required(N);
+    vector<int> additional(N);
+
+    // 사용중인 메모리
+    for (int i = 0; i < N; i++) {
+        cin >> required[i];
+    }
+
+    // 추가적인 메모리
+    for (int i = 0; i < N; i++) {
+        cin >> additional[i];
+    }
+
+    // 모든 추가 메모리의 값을 더한 값
+    int total_cost = 0;
+    for (int i = 0; i < N; i++) {
+        total_cost += additional[i];
+    }
+
+    // 추가 메모리의 합을 기준으로 만든 dp
+    vector<int> dp(total_cost + 1, 0);
 
     for (int i = 0; i < N; i++) {
-        cin >> required_memory[i];
+        int cost = additional[i];  // 상태축(비용)에 더해지는 값
+        int mem = required[i];  // 최적화 대상
+
+        // 뒤에서 앞으로 갱신
+        for (int j = total_cost; j >= cost; j--) {
+            dp[j] = max(dp[j], dp[j - cost] + mem);
+        }
     }
 
-    for (int i = 0; i < N; i++){
-        cin >> additional_memory[i];
+    int answer = -1;
+    for (int i = 0; i <= total_cost; i++) {
+        if (dp[i] >= M) {
+            answer = i;
+            break;
+        }
     }
 
-    min_btye(0, 0, 0);
-    
-    cout << result;
+    cout << answer;
 
     return 0;
 }
