32a5a4e2ced32939a199c0e4800efc369dd27a22
박민식 <jaden2973@naver.com>
2026-01-16 13:58:30 +0900

    9205 cpp incomplete for daily baekjun 260116

--- cpp/gold5/Beer9205.cpp
+++ cpp/gold5/Beer9205.cpp
@@ -6,44 +6,50 @@
 using namespace std;
 
 int main(void){
-    int test_case;
+    int test_case; // 테스트 케이스
 
-    int cons;
-    int start_x, start_y;
+    int cons; // 편의점 수
+    int start_x, start_y; // 시작 위치
 
-    vector<pair<int, int>> con;
+    vector<pair<int, int>> con; // 편의점 배열
 
-    int end_x, end_y;
+    int end_x, end_y; // 도착 위치
 
-    string result = "sad";
+    string result = "sad"; // 결과값
 
     cin >> test_case;
 
     for (int i = 0; i < test_case; i++) {
-        cin >> cons;
-        cin >> start_x >> start_y;
+        cin >> cons;  // 편의점 수 받기
+        cin >> start_x >> start_y;  // 시작점 받기
 
+        // 편의점 좌표 저장
         for (int j = 0; j < cons; j++) {
             int con_x, con_y;
             cin >> con_x >> con_y;
             con.push_back(make_pair(con_x, con_y));
         }
 
+        // 도착 지점 저장
         cin >> end_x >> end_y;
 
+        // 방문 확인
         vector<bool> visited(cons, false);
 
+        // bfs를 위한 큐
         queue<pair<int, int>> q;
 
         q.push({start_x, start_y});
 
+        // 
         while(!q.empty()) {
             auto cur = q.front();
             q.pop();
             int x = cur.first;
             int y = cur.second;
 
-            if(abs(x - end_x) + abs(y - end_y)) {
+            if(abs(x - end_x) + abs(y - end_y) <= 1000) {
+                cout << "<<" << result << ',' << x << ',' << y << ">>";
                 result = "happy";
                 break;
             }
@@ -53,15 +59,13 @@ int main(void){
                 int next_x = next.first;
                 int next_y = next.second;
                 if (!visited[i] && (abs(x - next_x) + abs(y - next_y) <= 1000)) {
+                    cout << '('<< result << ','<< next_x << ',' << next_y << ')';
                     q.push({next_x, next_y});
                     visited[i] = true;
                 }
             }
-
         }
-
         cout << result << "\n";
-
     }
 
     return 0;
