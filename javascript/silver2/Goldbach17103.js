ec598bbf6e89c71a9c06bda4ab083b43cdb395fb
박민식 <jaden2973@naver.com>
2026-01-20 15:32:26 +0900

    imcomplete 17103 javascript

---
+++ javascript/silver2/Goldbach17103.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+const input = fs.readFileSync(0, 'utf8').trim()
+const lines = input.split('\n')
+
+const T = lines[0].split("\n").map(Number)
+
+let arr = []
+
+for (let i = 0; i < T; i++) {
+    const N = lines[i + 1].split("\n").map(Number)
+
+    
+}
