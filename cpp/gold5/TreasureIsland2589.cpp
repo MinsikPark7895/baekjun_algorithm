ce86d76538db0d613d007fcd5afe4cdb3c3f86b1
박민식 <jaden2973@naver.com>
2026-01-21 19:59:36 +0900

    2589 cpp incomplete 260121

---
+++ cpp/gold5/TreasureIsland2589.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+int bfs() {
+
+
+    return 0;
+}
+
+int main(void) {
+
+    int N, M;
+    cin >> N >> M;
+
+    vector<string> board(N);
+    vector<vector<int>> visited(N, vector<int>(M, 0));
+    vector<pair<int, int>> start;
+
+    for (int i = 0; i < N; i++) {
+        cin >> board[i];
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            int cur_row = i;
+            int cur_col = j;
+            int cnt = 0;
+            for (int k = 0; k < 4; k++) {
+                int next_row = cur_row + dy[k];
+                int next_col = cur_col + dx[k];
+                if (next_row >=0 && next_row < N && next_col >= 0 && next_col < M){
+                    if (board[cur_row][cur_col] == 'L' && board[next_row][next_col] == 'L') {
+                        cnt += 1;
+                    }
+                }
+
+            }
+            if (cnt == 1) {
+               start.push_back({cur_row, cur_col}); 
+            }
+        }
+    }
+
+    int result = 0;
+
+    for (int i = 0; i < start.size(); i++) {
+        auto cur = start[i];
+
+        int len = bfs();
+
+    }
+
+    return 0;
+}
