7ba6e0ecf9f0b2ab66b69c24c2ccce40fd02ebbb
박민식 <jaden2973@naver.com>
2026-01-22 10:17:40 +0900

    17103 javascript solved for daily baekjun 260122

--- javascript/silver2/Goldbach17103.js
+++ javascript/silver2/Goldbach17103.js
@@ -2,12 +2,51 @@ const fs = require('fs')
 const input = fs.readFileSync(0, 'utf8').trim()
 const lines = input.split('\n')
 
-const T = lines[0].split("\n").map(Number)
+const T = Number(lines[0])
 
-let arr = []
+let Ns = new Array(T)
+let maxN = 0
 
+// 모든 테스트케이스의 최대값부터 구하기
 for (let i = 0; i < T; i++) {
-    const N = lines[i + 1].split("\n").map(Number)
-
+    const N = Number(lines[i + 1])
+    Ns[i] = N
+    if(N > maxN){
+        maxN = N
+    }
     
 }
+
+// 최대 값 기준으로 소수 여부 판단
+let isPrime = new Array(maxN + 1).fill(true)
+if (maxN >= 0) {
+    isPrime[0] = false
+}
+if (maxN >= 1) {
+    isPrime[1] = false
+}
+
+for (let i = 2; i * i <= maxN; i++) {
+    if (!isPrime[i]) {
+        continue
+    }
+    for (let j = i * i; j <= maxN; j += i) {
+        isPrime[j] = false
+    }
+}
+
+let result = ''
+for (let i = 0; i < T; i++) {
+    const N = Ns[i]
+    let count = 0
+
+    for (let p = 2; p <= N / 2; p++) {
+        if (isPrime[p] && isPrime[N - p]) {
+            count++
+        }
+    }
+
+    result = result + String(count) + '\n'
+}
+
+console.log(result)
