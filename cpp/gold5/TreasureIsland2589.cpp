b603f9b680e6a99ee63375734766dc8eeb2a28ec
박민식 <jaden2973@naver.com>
2026-01-22 19:41:17 +0900

    2589 cpp incomplete

--- cpp/gold5/TreasureIsland2589.cpp
+++ cpp/gold5/TreasureIsland2589.cpp
@@ -7,7 +7,25 @@ using namespace std;
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 
-int bfs() {
+int bfs(int start_row, int start_col) {
+    queue<pair<int, int>> q;
+
+    q.push({start_row, start_col});
+
+    while (!q.empty()) {
+        auto now = q.front();
+        int row_now = now.first;
+        int col_now = now.second;
+        q.pop();
+
+        for(int i = 0; i < 4; i++) {
+            int nxt_row = row_now + dy[i];
+            int nxt_col = col_now + dx[i];
+            if (nxt_row >= 0 && nxt_col>=0) {
+                // 조건 다시 설정
+            }
+        }
+    }
 
 
     return 0;
