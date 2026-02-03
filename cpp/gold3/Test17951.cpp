48c055e255dc8fda6548b7f92a6e42565fea806b
박민식 <jaden2973@naver.com>
2026-02-03 23:26:14 +0900

    11722 js incomplete 260203

---
+++ cpp/gold3/Test17951.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> board;
+
+int main(void) {
+
+    int N;
+    int K;
+
+    board.resize(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> board[i];
+    }
+
+    
+
+    return 0;
+}
---
+++ javascript/silver2/Longest11722.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+const input = fs.readFileSync(0, 'utf8').trim().split('\n')
+
+
+const N = Number(input[0])
+const A = input[1].split(' ').map(Number)
+
+
+
