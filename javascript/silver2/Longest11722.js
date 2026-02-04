0071bbc77f7b841b82fa9d09ab4b6ebca28b8ec9
박민식 <jaden2973@naver.com>
2026-02-04 21:19:56 +0900

    11722 javascript solved for daily baekjun 260204

--- javascript/silver2/Longest11722.js
+++ javascript/silver2/Longest11722.js
@@ -1,9 +1,20 @@
 const fs = require('fs')
-const input = fs.readFileSync(0, 'utf8').trim().split('\n')
-
+// const input = fs.readFileSync(0, 'utf8').trim().split('\n')
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath).toString().split('\n');
 
 const N = Number(input[0])
 const A = input[1].split(' ').map(Number)
 
+const dp = new Array(N).fill(1)
 
+for (let i = 0; i < N; i++) { 
+    for(let j = 0; j < i; j++) {
+        if (A[j] > A[i]) {
+            dp[i] = Math.max(dp[i], dp[j] + 1)
+        }
+    }
+}
+
+console.log(Math.max(...dp))
 
