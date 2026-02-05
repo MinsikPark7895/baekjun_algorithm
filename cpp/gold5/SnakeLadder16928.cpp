5e9329124af82b8703e8aaa49950aa3602054129
박민식 <jaden2973@naver.com>
2026-02-05 22:42:19 +0900

    16928 cpp incomplete

--- cpp/gold5/SnakeLadder16928.cpp
+++ cpp/gold5/SnakeLadder16928.cpp
@@ -1,27 +1,57 @@
 #include <iostream>
 #include <map>
+#include <limits>
+#include <math.h>
 
 using namespace std;
 
 int main(void) {
-    int dp[100] = {0, };
+    int inf = numeric_limits<int>::max();
+    int dp[100] = {inf, };
 
     int N, M;
     cin >> N >> M;
 
-    for (int i = 0; i < 6; i++) {
-        dp[i] = 1;
-    }
-
-    map<int, int> dict;
+    map<int, int> dict_ladder;
+    map<int, int> dict_snake;
 
     int a, b;
     for (int i = 0; i < N; i++) {
         cin >> a >> b;
-        dict[a] = b;
+        dict_ladder[a] = b;
     }
 
-    
+    for(int i = 0; i < M; i++) {
+        cin >> a >> b;
+        dict_snake[a] = b;
+    }
+
+    for (int i = 0; i < N; i++){
+        if (dict_snake.find(i) != dict_snake.end()){
+            continue;
+        }
+        else if (i < 6) {
+            dp[i] = 1;
+        }
+        else {
+            for (int j = 0; j < 6; j++) {
+                if (dict_snake.find(i) != dict_snake.end()){
+                    continue;
+                }                
+                dp[i] = min(dp[i - j - 1] + 1, dp[i]);
+            }
+        }
+        // C++20이상은 contains() 사용
+        if (dict_ladder.find(i) != dict_ladder.end()) {
+            if (dict_snake.find(i) != dict_snake.end()){
+                continue;
+            }
+            dp[dict_ladder[i]] = dp[i];
+        }
+
+    }
+
+    cout << dp[99];
 
     return 0;
 }
