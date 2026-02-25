fde047388d9f4a5ffa0cfd0ad4162634f049b059
박민식 <jaden2973@naver.com>
2026-02-25 23:37:12 +0900

    5014 javascript solved for daily baekjun 260225

--- javascript/silver1/StartLink5014.js
+++ javascript/silver1/StartLink5014.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const input = fs.readFileSync(0, 'utf').trim().split(' ')
+const input = fs.readFileSync(0, 'utf8').trim().split(' ')
 
 const F = Number(input[0])
 const S = Number(input[1])
@@ -7,25 +7,37 @@ const G = Number(input[2])
 const U = Number(input[3])
 const D = Number(input[4])
 
+const visited = new Array(F + 1).fill(false);
+
+const queue = [[S, 0]]
+visited[S] = true;
+
 let result = "use the stairs"
-let cur = S
-let cnt = 0
+let head = 0  // 현재 확인하는 큐의 인덱스
 
-while (cur != G) {
-    cnt++
-    let next_U = cur + U
-    let next_D = cur - D
-    if (cur < G) {
-        if (next_U == G) {
-            result = cnt
-            break
-        }
-        else if (next_U > G) {
-            
-        }
-    }
-    else if(cur > G) {
+// BFS
+while (head < queue.length) {
+    const [cur, cnt] = queue[head++]
 
+    // 목표 층에 도달한 경우
+    if (cur === G) {
+        result = cnt
+        break
     }
-    
+
+    // 위로 가는 경우
+    const nextUp = cur + U
+    if (nextUp <= F && !visited[nextUp]) {
+        visited[nextUp] = true;
+        queue.push([nextUp, cnt + 1])
+    }
+
+    // 아래로 가는 경우
+    const nextDown = cur - D
+    if (nextUp >= 1 && !visited[nextDown]) {
+        visited[nextDown] = true
+        queue.push([nextDown, cnt + 1])
+    }
 }
+
+console.log(result)
