2a72744ccc5dd4f2290c6dabad5e45fb30624cde
박민식 <jaden2973@naver.com>
2026-03-10 23:28:05 +0900

    14719 cpp solved for daily baekjun 260310

--- cpp/gold5/Raindish14719.cpp
+++ cpp/gold5/Raindish14719.cpp
@@ -13,6 +13,11 @@ int main()
     for (int i = 0; i < W; i++) {
         cin >> blocks[i];
     }
+    
+    if (W == 0) {
+        cout << 0;
+        return 0;
+    }
 
     stack<int> heights;
     stack<int> idx;
@@ -23,18 +28,26 @@ int main()
     int result = 0;
 
     for (int i = 1; i < W; i++) {
-        while (true) {
-            if (heights.top() < blocks[i]) {
-                result += (blocks[i] - heights.top()) * (i - idx.top());
-                heights.pop();
-                idx.pop();
-            }
-            else if (blocks.empty() || heights.top() >= blocks[i]) {
-                heights.push(blocks[i]);
-                idx.push(i);
+        while (!heights.empty() && heights.top() < blocks[i]) {
+            int bottom_height = heights.top();
+            heights.pop();
+            idx.pop();
+            
+            // 스택이 비어있으면 
+            if (heights.empty()){
                 break;
             }
+            
+            int width = i - idx.top() - 1;
+            
+            // 곱하는 건 좌우 높이 중 최소 값에서 아래 바닥을 뺀 값임 
+            int trapped_height = min(blocks[i], heights.top()) - bottom_height;
+        
+            result += width * trapped_height;
         }
+        
+        heights.push(blocks[i]);
+        idx.push(i);
     }
 
     cout << result;
