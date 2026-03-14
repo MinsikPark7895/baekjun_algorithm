85c852323bd669f28e92406469743f81e09df772
박민식 <jaden2973@naver.com>
2026-03-15 00:05:50 +0900

    2504 cpp first code

--- cpp/gold5/Parenthsis2504.cpp
+++ cpp/gold5/Parenthsis2504.cpp
@@ -7,51 +7,74 @@ using namespace std;
 
 int main(void) {
     string str;
-    cin >>> str;
+    cin >> str;
     
     stack<string> st;
     stack<int> num;
     
-    int result = 0;
-    
-    for (int i = 0; i < st.size(); i++) {
-        if (stack.empty()) {
-            st.push(str[i]);
-        } 
-        else if (stack.top() == "(" {
-            if (str[i] == ")") {
-                if (i >= 1 && str[i - 1] == "(" ) {
-                    result += 2;
-                    st.pop();
-                }
-                else if (i >= 1 && str[i - 1] != "(") {
-                    result *= 2;
-                    st.pop();
-                }
-            }
-            // (에서 ]가 올 수 없음 
-            else if (str[i] == "]") {
-                result = 0;
+    for (int i = 0; i < str.size(); i++) {
+        // 여는 괄호일 때
+        if (str[i] == '(') {
+            st.push(string(1, str[i]));
+        }
+        else if (str[i] == '[') {
+            st.push(string(1, str[i]));
+        }
+        // 닫는 소괄호일 때 ')'
+        else if (str[i] == ')') {
+            if (st.empty()) {
+                num.push(0); 
                 break;
             }
-            
-        }
-        else if (stack.top() == "["){
-            if (str[i] == "[") {
-                if (i >= 1 && str[i - 1] == "[") {
-                    result += 3;
-                    st.pop();
-                }
-                else if (i >= 1 && str[i - 1] != "[") {
-                    result *= 3;
-                }
+            if (i >= 1 && str[i - 1] == '(') {
+                num.push(2);
+                st.pop();
             }
-            else if (str[i] == '')
+            else if (i >= 1 && (str[i - 1] == ')' || str[i - 1] == ']')) {
+                // num 스택에서 2개를 빼서 더하고 2를 곱한 후 다시 넣기
+                int a = num.top();
+                num.pop();
+                int b = num.top();
+                num.pop();
+                num.push(2 * (a + b));
+                st.pop();
+            }
+            else if (i >= 1 && str[i - 1] == '['){
+                num.push(0); // 짝이 맞지 않는 실패 케이스
+                break;
+            }
         }
-        
+        // 닫는 대괄호일 때 ']' (새로 구현된 부분)
+        else if (str[i] == ']') {
+            if (st.empty()) {
+                num.push(0);
+                break;
+            }
+            if (i >= 1 && str[i - 1] == '[') {
+                num.push(3);
+                st.pop();
+            }
+            else if (i >= 1 && (str[i - 1] == ']' || str[i - 1] == ')')) {
+                // num 스택에서 2개를 빼서 더하고 3을 곱한 후 다시 넣기
+                int a = num.top();
+                num.pop();
+                int b = num.top();
+                num.pop();
+                num.push(3 * (a + b));
+                st.pop();
+            }
+            else if (i >= 1 && str[i - 1] == '('){
+                num.push(0); // 짝이 맞지 않는 실패 케이스
+                break;
+            }
+        }
     }
     
-    
+    if (num.empty()) {
+        cout << 0 << "\n";
+    } else {
+        cout << num.top() << "\n";
+    }
     
     return 0;
 }
