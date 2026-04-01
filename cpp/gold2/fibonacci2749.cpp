7b5413def3b8a1baf70969b73f332256bf03c64c
박민식 <jaden2973@naver.com>
2026-04-01 14:04:09 +0900

    2749 cpp solved for daily baekjun 260401

--- cpp/gold2/fibonacci2749.cpp
+++ cpp/gold2/fibonacci2749.cpp
@@ -3,42 +3,36 @@
 
 using namespace std;
 
-// 순환 큐
-vector<int> arr(10, -1);
+// 순환 큐 => 피사노의 주기
+vector<int> arr(1500000, -1);
 
 // 피사노의 주기 사용
 int fibonnaci(int idx) {
     if (idx == 0) {
         arr[0] = 0;
-        return arr[0];
     }
     else if (idx == 1) {
         arr[1] = 1;
-        return arr[1];
     }
     else if (idx >= 2) {
-        if (arr[idx % 10] < arr[(idx - 1) % 10]) {
-            arr[idx % 10] = (fibonnaci(idx - 1) + fibonnaci(idx - 2)) % 1000000;
-            
+        if (arr[idx % 1500000] == -1) {
+            arr[idx % 1500000] = (fibonnaci(idx - 1) + fibonnaci(idx - 2)) % 1000000;
         }
-        return arr[idx];
-        
     }
-
-    // 모든 조건문을 빠져나올 경우
-    return 0;
-    
+    return arr[idx % 1500000];            
 }
 
 int main(void) {
     
     long long N;
     cin >> N;
+
+    int target = N % 1500000;
     
     arr[0] = 0;
     arr[1] = 1;
     
-    cout << fibonnaci(N);
+    cout << fibonnaci(target);
        
 
     
