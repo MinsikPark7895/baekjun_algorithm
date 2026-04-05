4401508cae32fc9425cfa285ef5671c3b7a6d200
박민식 <jaden2973@naver.com>
2026-04-05 19:53:24 +0900

    1715 cpp solved for daily baekjun 260405

--- cpp/gold4/CardSort1715.cpp
+++ cpp/gold4/CardSort1715.cpp
@@ -1,50 +1,65 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <algorithm>
 
 using namespace std;
 
-void merge(int left, int right) {
-    int mid = (left + right) / 2;
-    
-    int i = left;
-    int j = mid + 1;
-    int k = left;
-    while (i <= mid && j <= right) {
-        if (arr[i] <= arr[j]) {
-            arr2[k++] = arr[i++];
-        }
-        else {
-            
-        }
-    }
-}
-
-void merge_sort(int left, int right){
-    
-    int mid;
-    if (left < right) {
-        mid = (left + right) / 2;
-        merge_sort(left, mid);
-        merge_sort(mid + 1, right);
-        merge(left, right);
-    }
-}
-
 int main(void) {
-    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     
     int N;
-    vector<int> arr;
-    
     cin >> N;
-    arr.assign(N);
+    
+    vector<int> cards;
+    cards.resize(N);
     
     for (int i = 0; i < N; i++) {
-        
-        cin >> arr[i];
+        cin >> cards[i];
     }
     
+    sort(cards.begin(), cards.end());
     
+    queue<int> q1;
+    queue<int> q2;
+    
+    
+    for (int i = 0; i < N; i++ ){
+        q1.push(cards[i]);
+    }
+    
+    int total_cost = 0;
+    
+    for (int i = 0; i < N - 1; i++) {
+        int sum = 0;
+        
+        for (int j = 0; j < 2; j++) {
+            int val;
+            if (q1.empty()) {
+                val = q2.front();
+                q2.pop();
+            }
+            else if (q2.empty()) {
+                val = q1.front();
+                q1.pop();
+            }
+            else if (q1.front() < q2.front()) {
+                val = q1.front();
+                q1.pop();
+            }
+            else {
+                val = q2.front();
+                q2.pop();
+            }
+            sum += val;
+        }
+        total_cost += sum;
+        
+        q2.push(sum);
+    }
+    
+    cout << total_cost << "\n";
     
     return 0;
 }
